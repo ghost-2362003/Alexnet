b7bc52ffd3405b6356b2045f9c5e9a939a247f8e
Shubhojyoti Das <shubhojyotidas800@gmail.com>
2024-11-20 23:01:45 +0530

    add all the files

---
+++ training and testing/train.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+from DataLoader import DataLoader as dl
+from AlexNet_Architecture import AlexNet
+
+#setup the available device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+num_classes = 10
+num_epochs = 20
+batch_size = 64
+learning_rate = 0.005
+model = AlexNet(num_classes).to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay = 0.005, momentum = 0.9)  
+    
+# Train the model
+train_loader, valid_loader = dl.get_train_valid_loader(data_dir = './data', batch_size = 64, augment = False, random_seed = 1)
+
+total_step = len(train_loader)
+
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):  
+        # Move tensors to the configured device
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+
+        # Validation
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in valid_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            del images, labels, outputs
+
+        print('Accuracy of the network on the {} validation images: {} %'.format(5000, 100 * correct / total))
